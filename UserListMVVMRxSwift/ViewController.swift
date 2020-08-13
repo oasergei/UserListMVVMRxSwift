ca67e7015008a99686562a34c0ebaff99219f6bf
oasergei <oasergei@rambler.ru>
2020-08-13 16:56:00 +0500

    Initial Commit

---
+++ UserListMVVMRxSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UserListMVVMRxSwift
+//
+//  Created by Sergey on 13.08.2020.
+//  Copyright © 2020 Sergey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
